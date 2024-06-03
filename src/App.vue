053663a5148037a80af453a56badee434c3e5edb
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-06-03 02:50:04 -0600

    created nav bar component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-
-      <nav>
-        <p class="">HOME</p>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NavBar />
   </header>
 
   <RouterView />
---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar navbar-expand-sm sticky-top bg-secondary text-light">
+    <div class="container-fluid">
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+        </div>
+      </div>
+
+      <div class="navbar-brand">
+        <!-- <i class="mdi mdi-crystal-ball pe-2"></i> -->
+        <a class="fs-3 fw-bold" href="#">Kyle Peppersack</a>
+      </div>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup2"
+        aria-controls="navbarNavAltMarkup2" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup2">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home2</a>
+          <a class="nav-link" href="#">Features2</a>
+          <a class="nav-link" href="#">Pricing2</a>
+        </div>
+      </div>
+
+    </div>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.navbar-brand {
+  color: var(--bs-primary);
+
+  >a {
+    color: var(--bs-primary);
+  }
+}
+</style>
