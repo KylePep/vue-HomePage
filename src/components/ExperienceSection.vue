c01e2facd658566444221e5b6f3ae87ad62e478f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-06-10 02:18:51 -0600

    hover effect added to exp img

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -7,7 +7,7 @@
 
       <div class="col-12 col-md-4 mb-5 mb-md-0 ">
         <div class="card bg-secondary text-light h-100">
-          <img src="../assets/images/business.jpg" class="card-img-top" alt="...">
+          <img src="../assets/images/frontEnd.png" class="card-img" alt="...">
           <div class="card-body d-flex flex-column justify-content-between align-items-center">
             <h5 class="card-title">Front End</h5>
             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quod.</p>
@@ -20,7 +20,7 @@
 
       <div class="col-12 col-md-4 mb-5 mb-md-0">
         <div class="card bg-success h-100">
-          <img src="../assets/images/internet.jpg" class="card-img-top" alt="...">
+          <img src="../assets/images/frontEnd.png" class="card-img" alt="...">
           <div class="card-body d-flex flex-column justify-content-between align-items-center">
             <h5 class="card-title">Back End</h5>
             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quod.</p>
@@ -33,7 +33,7 @@
 
       <div class="col-12 col-md-4 mb-5 mb-md-0">
         <div class="card  bg-secondary text-light h-100">
-          <img src="../assets/images/learning.jpg" class="card-img-top" alt="...">
+          <img src="../assets/images/frontEnd.png" class="card-img" alt="...">
           <div class="card-body d-flex flex-column justify-content-between align-items-center">
             <h5 class="card-title">UI Design</h5>
             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quod.</p>
@@ -53,6 +53,14 @@
   border-radius: 50%;
   width: 80px;
   height: 80px;
+}
+
+.card-img {
+  transition: all .5s ease-in-out;
+}
+
+.card-img:hover {
+  scale: 1.05;
 }
 
 .card-title {
