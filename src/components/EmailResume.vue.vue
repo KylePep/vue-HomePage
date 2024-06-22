d12ce7c23a3e467a2381d68de2a5de6ea5de7f00
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-06-22 03:07:34 -0600

    Created EmailResume.vue moved code for button into it

---
+++ src/components/EmailResume.vue.vue
@@ -0,0 +1,56 @@
+<script setup></script>
+
+
+<template>
+  <form class="row g-3">
+
+    <div class="col-12 col-md-6">
+      <label for="exampleInputEmail1" class="form-label d-none ">Email address</label>
+      <input type="email" class="form-control py-3 fs-5 rounded-1" id="exampleInputEmail1" aria-describedby="emailHelp"
+        placeholder="EMAIL">
+    </div>
+
+    <div class="col-12 col-md-6">
+      <button type="submit" class="w-100 btn-hero rounded-1 py-3 fs-5 fw-semibold">GET MY
+        RESUME</button>
+    </div>
+
+    <div id="emailHelp" class="text-light col-12 col-md-6 mt-3">* Get my resume directly in your mailbox</div>
+  </form>
+</template>
+
+
+<style lang="scss" scoped>
+.btn-hero {
+  display: inline-block;
+  padding: 0.9rem 1.8rem;
+  font-size: 16px;
+  font-weight: 700;
+  color: var(--bs-light);
+  border: 3px solid var(--bs-light);
+  cursor: pointer;
+  position: relative;
+  background-color: transparent;
+  text-decoration: none;
+  overflow: hidden;
+  z-index: 1;
+  font-family: inherit;
+}
+
+.btn-hero::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--bs-success);
+  transform: translateX(-100%);
+  transition: all .3s;
+  z-index: -1;
+}
+
+.btn-hero:hover::before {
+  transform: translateX(0);
+}
+</style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AnimatedBackground from './AnimatedBackground.vue'
+import EmailResume from './EmailResume.vue.vue'
 </script>
 
 <template>
@@ -22,21 +23,7 @@ import AnimatedBackground from './AnimatedBackground.vue'
 
             </div>
 
-            <form class="row g-3">
-
-              <div class="col-12 col-md-6">
-                <label for="exampleInputEmail1" class="form-label d-none ">Email address</label>
-                <input type="email" class="form-control py-3 fs-5 rounded-1" id="exampleInputEmail1"
-                  aria-describedby="emailHelp" placeholder="EMAIL">
-              </div>
-
-              <div class="col-12 col-md-6">
-                <button type="submit" class="w-100 btn-hero rounded-1 py-3 fs-5 fw-semibold">GET MY
-                  RESUME</button>
-              </div>
-
-              <div id="emailHelp" class="text-light col-12 col-md-6 mt-3">* Get my resume directly in your mailbox</div>
-            </form>
+            <EmailResume />
 
           </div>
 
@@ -74,39 +61,6 @@ h2 {
   text-align: center;
   color: var(--bs-light);
   padding: 0;
-}
-
-.btn-hero {
-  display: inline-block;
-  padding: 0.9rem 1.8rem;
-  font-size: 16px;
-  font-weight: 700;
-  color: var(--bs-light);
-  border: 3px solid var(--bs-light);
-  cursor: pointer;
-  position: relative;
-  background-color: transparent;
-  text-decoration: none;
-  overflow: hidden;
-  z-index: 1;
-  font-family: inherit;
-}
-
-.btn-hero::before {
-  content: "";
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: var(--bs-success);
-  transform: translateX(-100%);
-  transition: all .3s;
-  z-index: -1;
-}
-
-.btn-hero:hover::before {
-  transform: translateX(0);
 }
 
 @media screen and (min-width: 768px) {
